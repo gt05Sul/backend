3d94b888ad27c503ead66359084fa179182cf127
Carlos Vieira <carlos.vieira@digitalcollegesul.local>
2024-11-01 09:00:46 -0300

    feat: validator and existingUser

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,6 @@
+const { where } = require('sequelize');
 const { User } = require('../models');
+const validator = require('validator');
 
 const getAllUsers = async (_req, res) => {
     const users = await User.findAll();
@@ -11,12 +13,16 @@ const createUser = async (req, res) => {
     }
     const {name, email, password} = req.body;
     
-    if (!name || !email || !password) {
-        return res.status(400).json({ error: "Nome, email e senha são obrigatórios." })
+    if (!validator.isEmail(email)) {
+        return res.status(400).json({ error: "Formato do e-mail inválido." })
     }
     try {
+        const existingUser = await User.findOne({ where: { email } });
+        if (existingUser) {
+            return res.status(400).json({ error: "e-mail já cadastrado" })
+        }
         const newUser = await User.create({ name, email, password });
-        res.status(201).json(newUser);
+        res.status(201).json({ message: `Seja bem vindo ${name}` });
     } catch (error) {
         res.status(500).json({ error: "Erro ao criar usuário" })
     }
